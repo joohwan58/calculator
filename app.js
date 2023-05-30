e606bfb3468fb30f655ca5a302b275655471e47c
Kim Joohwan <kim.joohwan58@gmail.com>
2023-05-30 14:55:49 +0900

    added object to deal with string to number parsing and evaluation

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-let calculatorScreen = {
+const calculatorScreen = {
     upper: document.querySelector('.upper-screen'),
     lower: document.querySelector('.lower-screen'),
 
@@ -17,11 +17,45 @@ let calculatorScreen = {
         calculatorScreen.lower.textContent = originalContent + append;
     },
     updateUpperScreen: (update) => {
-        calculatorScreen.upper.clearUpperScreen
+        calculatorScreen.upper.clearUpperScreen();
         calculatorScreen.upper.textContent = update;
     },
     updatelowerScreen: (update) => {
-        calculatorScreen.lower.clearLowerScreen
+        calculatorScreen.lower.clearLowerScreen();
         calculatorScreen.lower.textContent = update;
     }
+}
+
+let evaluation = {
+    firstNumber: "",
+    operator: "",
+    secondNumber: "",
+
+    evaluate: () => {
+        let a = parseFloat(evaluation.firstNumber);
+        let b = parseFloat(evaluation.secondNumber);
+        switch (evaluation.operator) {
+            case '÷':
+                if (b == 0) {
+                    return 'LMAO';
+                }
+                return a / b;
+            case '×':
+                return a * b;
+            case '-':
+                return a - b;
+            case '+':
+                return a + b;
+            default:
+                return 'ERROR';
+                break;
+        }
+    },
+
+    appendFirstNumber: (n) => {
+        evaluation.firstNumber += n;
+    },
+    appendSecondNumber: (n) => {
+        evaluation.secondNumber += n;
+    },
 }
